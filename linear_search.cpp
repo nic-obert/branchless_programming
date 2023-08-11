feeb4b757756dadff739aca445e627dd06da949e
Nicholas Obert <nchlsuba@gmail.com>
2023-08-11 11:29:08 +0200

    added some more comments

--- linear_search.cpp
+++ linear_search.cpp
@@ -6,11 +6,12 @@ int recursive_search(const char* bytes, unsigned int index, unsigned int length,
 
     typedef int (*SearchF)(const char*, unsigned int, unsigned int, char);
 
+    /// A Local struct to contain the case handler functions
     struct Local {
 
         /// Bytes are not equal, continue the search
         static int not_equal(const char* bytes, unsigned int index, unsigned int length, char target) {
-            // Increment the index
+            // Increment the index to check the next character
             index ++;
 
             struct Local {
@@ -28,7 +29,7 @@ int recursive_search(const char* bytes, unsigned int index, unsigned int length,
 
             SearchF cases[2] = { Local::not_equal, Local::equal };
 
-            // Check if the byte array is finished
+            // Check if the byte array is finished and call the handler function
             return (*cases[index == length])(bytes, index, length, target);
         }
 
@@ -39,14 +40,19 @@ int recursive_search(const char* bytes, unsigned int index, unsigned int length,
 
     };
 
+    // Create an array of function pointers to the case handler functions
     SearchF cases[2] = { Local::not_equal, Local::equal };
 
-    // Check if the bytes are equal
+    // Check if the bytes are equal and call the handler function
     return (*cases[*(bytes + index) == target])(bytes, index, length, target);
 }
 
 
+/// Search for the specified target byte. 
+///
+/// Return the index of the char. Return -1 is not found.
 int linear_search(const char* bytes, unsigned int length, char target) {
+    // Start the recursive search and set the start index to 0
     return recursive_search(bytes, 0, length, target);
 }
 
@@ -54,9 +60,11 @@ int linear_search(const char* bytes, unsigned int length, char target) {
 int main() {
 
     const char* str = "Hi nic\0";
+    unsigned int length = strlen(str);
 
-    int index_of_c = linear_search(str, strlen(str), 'c');
+    int index_of_c = linear_search(str, length, 'c');
 
     std::cout << "Index of c is " << index_of_c << ", length is " << strlen(str) << std::endl;
 
 }
+
